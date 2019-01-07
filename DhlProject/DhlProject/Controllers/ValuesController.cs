77c3b068aab9de43fcdde127bd44d0d0e9544d3a
Anna Burgina <anna190390@mail.ru>
2019-01-07 11:48:08 +0300

    Initial commit

---
+++ DhlProject/DhlProject/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Targets.Wrappers;
+using System;
+using System.Web.Http;
+
+namespace DhlProject.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private static Logger _logger;
+
+        private const string WebServiceUrl = "http://localhost:49883/api/logerror";
+
+        // GET api/values
+        public string Get()
+        {
+            var config = new LoggingConfiguration();
+
+            var webServiceTarget =
+                new WebServiceTarget
+                {
+                    Url = new Uri(WebServiceUrl),
+                    Protocol = WebServiceProtocol.JsonPost
+                };
+
+            AsyncTargetWrapper asyncWebServiceTarget = new AsyncTargetWrapper(webServiceTarget);
+            config.AddTarget("asyncWebService", asyncWebServiceTarget);
+
+            webServiceTarget.Parameters.Add(new MethodCallParameter("ProjectName", "Packman"));
+            webServiceTarget.Parameters.Add(new MethodCallParameter("Level", "${level}"));
+            webServiceTarget.Parameters.Add(new MethodCallParameter("Text", "blabla"));
+
+            var rule = new LoggingRule("*", LogLevel.Debug, asyncWebServiceTarget);
+            config.LoggingRules.Add(rule);
+
+            LogManager.Configuration = config;
+
+            _logger = LogManager.GetCurrentClassLogger();
+
+            _logger.Debug("Using programmatic config");
+
+            return "Hello world!";
+        }
+    }
+}
---
+++ LogService/LogService/Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+
+namespace LogService.Controllers
+{
+    public class LogsController : ApiController
+    {
+        [Route("api/logerror")]
+        public IHttpActionResult Post([FromBody]Error error)
+        {
+            var path = HttpContext.Current.Server.MapPath("~") + "\\logs.txt";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"time of record:{DateTime.Now} error:{error.ProjectName} {error.Level} {error.Text}");
+            }
+
+            return Ok();
+        }
+
+        public class Error
+        {
+            public string ProjectName { get; set; }
+            public string Text { get; set; }
+            public string Level { get; set; }
+        }
+    }
+}
